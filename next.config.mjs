e8d36078e6f7137c73981232a1a233f3f4080d3c
Tingly <pi@piprograms.io>
2024-08-03 14:59:46 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -10,11 +10,3 @@ const nextConfig = {
 };
 
 export default nextConfig;
-
-// NEXT_PUBLIC_API_KEY=AIzaSyCgizqF8am_Ol-i35g3BJp-J9mXmUyed3I
-// NEXT_PUBLIC_AUTH_DOMAIN=edited-pantry-tracker-2.firebaseapp.com
-// NEXT_PUBLIC_PROJECT_ID=edited-pantry-tracker-2
-// NEXT_PUBLIC_STORAGE_BUCKET=edited-pantry-tracker-2.appspot.com
-// NEXT_PUBLIC_MESSAGING_SENDER_ID=766361007562
-// NEXT_PUBLIC_APP_ID=1:766361007562:web:abf1d33e1feab2a6500b33
-// NEXT_PUBLIC_MEASUREMENT_ID=G-YJBF49LT5T
