a16f56c86b7915632bc274e787f65f2e843cf9df
tingly <pi@piprograms.io>
2024-08-04 09:24:32 -0400

    Update OutOfStock.js

--- app/components/OutOfStock.js
+++ app/components/OutOfStock.js
@@ -1,8 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import HighlightedText from "./HighlightedText";
-
+import parse from "html-react-parser";
 import { highlightText } from "../utils/textHighLight";
 
 import {
